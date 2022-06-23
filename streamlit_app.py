8f918ae912b5c69b4256061cbe8745d5de7e5406
Sarah Haley <sarahhaleyyy@gmail.com>
2022-06-22 20:22:24 -0700

    Update streamlit_app.py
    Set the index to the table to Fruit titles
    Added a pick list so customers could pick the fruit in their smoothies
    The pick list is by fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,10 @@ streamlit.title("🍌🍓 Build Your Own Fruit Smoothie 🥝🍇")
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Adding a Pick List here so they can pick what they want to include in their smoothie
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#display the table on the page
 streamlit.dataframe(my_fruit_list)
