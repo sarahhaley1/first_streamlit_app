48d6663c505e27ae9adebe1826a9b998d63b92e4
Sarah Haley <sarahhaleyyy@gmail.com>
2022-06-22 20:29:52 -0700

    Update streamlit_app.py
    Created a variable to hold the fruits selected by the customer
    added a filter that would only show the fruits selected by the customer on the table below
    displayed table with such edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,13 @@ streamlit.title("🍌🍓 Build Your Own Fruit Smoothie 🥝🍇")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Adding a Pick List here so they can pick what they want to include in their smoothie
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+# Adding a Pick List here so they can pick what they want to include in their smoothie 
+# Adding an automated example for display 
+# created a variable to hold the fruits selected
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+#filtering the table to only show fruits selected 
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
