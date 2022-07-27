167ce7f15c6b8b9a93c304fc07015ac9e8483782
Sarah Haley <sarahhaleyyy@gmail.com>
2022-07-26 19:38:03 -0700

    Add Kiwi to Fruit Advice Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 ## REMOVED ##streamlit.text(fruityvice_response.json())
 
