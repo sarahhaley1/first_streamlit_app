45d3b6421c111e37d251ab56f136d93e7a8915da
Sarah Haley <sarahhaleyyy@gmail.com>
2022-07-27 10:57:54 -0700

    removing an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,4 +54,3 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
 
-my_fruit_response = requests.get(my_data_rows + add_my_fruit)
