f3f387f114f6a4039b9d22d40ed486d5834887e3
Sarah Haley <sarahhaleyyy@gmail.com>
2022-07-26 19:41:01 -0700

    Adding choice box
    Adding a selection box for which type of fruits customers want information on

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,10 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kwiw')
+streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 ## REMOVED ##streamlit.text(fruityvice_response.json())
 
