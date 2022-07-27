9a16ffcc7bf0c2632bcfaf10b0c2f58e39180376
Sarah Haley <sarahhaleyyy@gmail.com>
2022-07-26 19:25:35 -0700

    Add Fruityvice API Response to Menu
    Added fruityvice api response to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -23,6 +24,12 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 
 #filtering the table to only show fruits selected 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
